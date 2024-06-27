434e42ee6bfca0144b304fa340c39f43f10700f6
SashaRybalkina <155681256+SashaRybalkina@users.noreply.github.com>
2024-06-27 12:10:11 -0600

    Added chat import to _layout.tsx

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -2,6 +2,8 @@ import { Slot, SplashScreen } from 'expo-router';
 
 import { AppProvider } from '../providers/AppProvider';
 
+import { Chat } from './Chat.js';
+
 void SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
