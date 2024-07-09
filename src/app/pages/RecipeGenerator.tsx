b646abbd7c38f46a94b5d64880e3f7611837a7b8
Emily <emily.silkin@gmail.com>
2024-07-09 10:28:21 -0700

    Some RecipeGenerator Edits

--- src/app/pages/RecipeGenerator.tsx
+++ src/app/pages/RecipeGenerator.tsx
@@ -10,7 +10,7 @@ import {
 } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import { NavigationProp } from '@react-navigation/native';
-import { Button, YStack } from 'tamagui';
+import { Button, XStack, YStack } from 'tamagui';
 
 type Props = {
   navigation: NavigationProp<any>;
@@ -27,9 +27,21 @@ const RecipeGenerator: React.FC<Props> = ({ navigation }) => {
 
   return (
     <View style={styles.container}>
-      <ImageBackground>
-        <View></View>
-      </ImageBackground>
+      <View>
+          <YStack padding={100}>
+            <Text style={styles.modalTitle}>Ok, now what are we working with?</Text>
+            <XStack>
+              <Text>Ingredients</Text>
+              <Text>include quantity (e.g. 2 eggs)</Text>
+            </XStack>
+
+            <TextInput
+              style={styles.input}
+              placeholder="1 gal of milk, 3 potatoes, 2 sticks of butter, etc"
+            />
+
+          </YStack>
+        </View>
       <View style={styles.buttons}>
         <Button style={styles.button}>
           <Ionicons name="create" size={40} color={'#FFF5CD'} />
@@ -67,6 +79,18 @@ const styles = StyleSheet.create({
     borderRadius: 0,
     borderWidth: 0,
     marginTop: 15,
+  },
+  modalTitle: {
+    fontSize: 24,
+    marginBottom: 20,
+    color: '#365E32',
+  }, 
+  input: {
+    width: '100%',
+    backgroundColor: '#F0F0F0',
+    padding: 10,
+    marginBottom: 20,
+    borderRadius: 5,
   },
 });
 
